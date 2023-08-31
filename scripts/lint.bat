bfd179a3dad50d02f9f0879c04bc749fcb403009
Isamu Mogi <isamu@leafytree.jp>
2023-08-31 20:13:00 +0900

    chore: setup pyright

--- scripts/lint.bat
+++ scripts/lint.bat
@@ -9,10 +9,10 @@ echo ### mypy ###
 call poetry run mypy --show-error-codes %pyi_files%
 echo ### flake8 ###
 call poetry run flake8 --count --show-source --statistics %py_files% %pyi_files%
-echo ### pyright ###
-call poetry run pyright %py_files% %pyi_files%
 echo ### pylint ###
 call poetry run pylint %py_files% %pyi_files%
+echo ### pyright ###
+call poetry run pyright %py_files% %pyi_files%
 popd
 endlocal
 endlocal
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -8,3 +8,4 @@ git ls-files "*.sh" | xargs shellcheck
 git ls-files "*.pyi" | xargs poetry run mypy --show-error-codes
 git ls-files "*.py" "*.pyi" | xargs poetry run flake8 --count --show-source --statistics
 git ls-files "*.py" "*.pyi" | xargs poetry run pylint
+git ls-files "*.py" "*.pyi" | xargs poetry run pyright
