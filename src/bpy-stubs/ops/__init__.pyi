2ee10e05a6f1bc78ccc29e085d99a91b4f97e236
Isamu Mogi <isamu@leafytree.jp>
2023-11-22 20:02:14 +0900

    chore: ignore A001

--- src/bpy-stubs/ops/__init__.pyi
+++ src/bpy-stubs/ops/__init__.pyi
@@ -2,7 +2,7 @@ from . import export_scene as export_scene
 from . import icyp as icyp
 from . import import_scene as import_scene
 from . import mesh as mesh
-from . import object as object  # noqa: A001
+from . import object as object
 from . import outliner as outliner
 from . import preferences as preferences
 from . import uv as uv
