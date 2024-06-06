82938a6ed284320c52eec60db88ceda8deacb030
Isamu Mogi <isamu@leafytree.jp>
2024-06-07 03:45:28 +0900

    feat: bpy.app.debug

--- src/bpy-stubs/app/__init__.pyi
+++ src/bpy-stubs/app/__init__.pyi
@@ -2,6 +2,7 @@ from . import handlers as handlers
 from . import timers as timers
 from . import translations as translations
 
+debug: bool
 version_cycle: str
 version: tuple[int, int, int]
 binary_path: str
